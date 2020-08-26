afe40b99efe9f354f32c10b0813394f993497888
Dong <thiendong.iuh@gmail.com>
2020-08-26 23:05:45 +0700

    remove js of element has removed

--- iot/dashboard/js/morphDash.js
+++ iot/dashboard/js/morphDash.js
@@ -235,29 +235,23 @@ function nav(page,btn){
 setInterval(function() {
   line1.append(new Date().getTime(), parseInt(el('hum').innerHTML));
   line2.append(new Date().getTime(), parseInt(el('temp').innerHTML));
-  line3.append(new Date().getTime(), parseFloat(el('ssx').innerHTML));
+  //line3.append(new Date().getTime(), parseFloat(el('ssx').innerHTML));
 }, 500);
 
 // Add to SmoothieChart
 
 
-      function createTimeline() {
-        var chart = new SmoothieChart({responsive: true});
-        chart.addTimeSeries(line1,{ strokeStyle:'#3F88BF', fillStyle:'rgba(77, 157, 218, 0.68)', lineWidth:2 });
-        chart.addTimeSeries(line2,{ strokeStyle:'#E21C33', fillStyle:'rgba(226, 28, 51, 0.58)', lineWidth:2 });
+   function createTimeline() {
+      var chart = new SmoothieChart({responsive: true});
+      chart.addTimeSeries(line1,{ strokeStyle:'#3F88BF', fillStyle:'rgba(77, 157, 218, 0.68)', lineWidth:2 });
+      chart.addTimeSeries(line2,{ strokeStyle:'#E21C33', fillStyle:'rgba(226, 28, 51, 0.58)', lineWidth:2 });
 
-        chart.streamTo(document.getElementById("chart"), 500);
-      }
+      chart.streamTo(document.getElementById("chart"), 500);
+   }
 
-      function createSoundChart(){
-      	var schart = new SmoothieChart({responsive:true});
-      	schart.addTimeSeries(line3,{ strokeStyle:'#3F88BF', fillStyle:'rgba(77, 157, 218, 0.68)', lineWidth:2 });
-
-        schart.streamTo(document.getElementById("soundchart"), 500);
-      }
 
 
 window.onload = () =>{
 	createTimeline();
-	createSoundChart();
+	// createSoundChart();
 }      
